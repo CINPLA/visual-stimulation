bf6a460d0f8b03e9f8526d22e81ad3d8c320a1fa
Milad <miladh@ibv.uio.no>
2018-11-16 11:34:52 +0100

    added raster

--- visualstimulation/plot.py
+++ visualstimulation/plot.py
@@ -1,10 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def polar_tuning_curve(orients, rates, ax=None, params={}):
     """
     Direction polar tuning curve
     """
-    import numpy as np
     import math
-    import pretty_plotting
 
     assert len(orients) == len(rates)
 
@@ -72,6 +74,7 @@ def orient_raster_plots(trials):
         list of neo.SpikeTrain
     """
     import seaborn
+    from visualstimulation.analysis import make_orientation_trials
     orient_trials = make_orientation_trials(trials)
     col_count = 4
     row_count = int(np.ceil(len(orient_trials))/col_count)
@@ -84,3 +87,52 @@ def orient_raster_plots(trials):
     fig.tight_layout()
 
     return fig
+
+
+def plot_raster(trials, color="#3498db", lw=1, ax=None, marker='.', marker_size=10,
+                ylabel='Trials', id_start=0, ylim=None):
+    """
+    Raster plot of trials
+    Parameters
+    ----------
+    trials : list of neo.SpikeTrains
+    color : color of spikes
+    lw : line width
+    ax : matplotlib axes
+    Returns
+    -------
+    out : axes
+    """
+    from matplotlib.ticker import MaxNLocator
+    if ax is None:
+        fig, ax = plt.subplots()
+    trial_id = []
+    spikes = []
+    dim = trials[0].times.dimensionality
+    for n, trial in enumerate(trials):  # TODO what about empty trials?
+        n += id_start
+        spikes.extend(trial.times.magnitude)
+        trial_id.extend([n]*len(trial.times))
+    if marker_size is None:
+        heights = 6000./len(trials)
+        if heights < 0.9:
+            heights = 1.  # min size
+    else:
+        heights = marker_size
+    ax.scatter(spikes, trial_id, marker=marker, s=heights, lw=lw, color=color,
+               edgecolors='face')
+    if ylim is None:
+        ax.set_ylim(-0.5, len(trials)-0.5)
+    elif ylim is True:
+        ax.set_ylim(ylim)
+    else:
+        pass
+    y_ax = ax.axes.get_yaxis()  # Get X axis
+    y_ax.set_major_locator(MaxNLocator(integer=True))
+    t_start = trials[0].t_start.rescale(dim)
+    t_stop = trials[0].t_stop.rescale(dim)
+    ax.set_xlim([t_start, t_stop])
+    ax.set_xlabel("Times [{}]".format(dim))
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+    return ax
